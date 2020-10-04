fb632e5e9b19e36c077e469091cc06a102803d73
Kaung Htet Paing <kaunghtetpaing199@gmail.com>
2020-10-04 21:12:00 +0630

    Updates

---
+++ precache-manifest.cff213246647945c684e1da665ed940d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f3a4946a29c19473a62e74c71dd288a",
+    "url": "/redux-shopping-card/index.html"
+  },
+  {
+    "revision": "75d6f89be15f1334949c",
+    "url": "/redux-shopping-card/static/css/main.d18dee0f.chunk.css"
+  },
+  {
+    "revision": "78539689b19346092350",
+    "url": "/redux-shopping-card/static/js/2.31d8aa1e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-shopping-card/static/js/2.31d8aa1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75d6f89be15f1334949c",
+    "url": "/redux-shopping-card/static/js/main.e74b708c.chunk.js"
+  },
+  {
+    "revision": "c3075532a2efdb9811e3",
+    "url": "/redux-shopping-card/static/js/runtime-main.304c9d77.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/redux-shopping-card/static/media/logo.8dad8028.svg"
+  }
+]);
